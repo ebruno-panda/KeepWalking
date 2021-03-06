530a660735bf8ba58fa05cae0ad939df130ca9ec
owner <owner@DESKTOP-5KT47FQ.mshome.net>
2021-03-05 23:11:02 -0500

    First Try

---
+++ Assignment4/src/org/howard/edu/lsp/assignment4/implementation/IntegerSetException.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package org.howard.edu.lsp.assignment4.implementation;
+
+/**
+ * @author owner
+ *
+ */
+public class IntegerSetException extends Exception{
+	/**
+	 * This class is solely dedicated to throwing exception type IntegerSetException
+	 */
+	
+	public IntegerSetException(String note) {
+		super(note);
+	}
+}
---
+++ Assignment4/src/org/howard/edu/lsp/assignment4/test/Captain.java
@@ -0,0 +1,149 @@
+/**
+ * 
+ */
+package org.howard.edu.lsp.assignment4.test;
+
+import org.howard.edu.lsp.assignment4.implementation.IntegerSet;
+import org.howard.edu.lsp.assignment4.implementation.IntegerSetException;
+
+/**
+ * @author Esterie
+ *
+ * Synopsis
+ * --------
+ * This Driver class, called Captain, calls the different functions from the IntegerSet class
+ * and tests different input scenarios for experimental purposes
+ */
+public class Captain {
+
+	/**
+	 * @param args
+	 * @throws IntegerSetException 
+	 */
+	
+	/*
+	 * Main method containing print statements with different input scenarios 
+	 * CHANGE VALUES AS YOU PLEASE
+	 */
+	
+	public static void main(String[] args) throws IntegerSetException {
+		// TODO Auto-generated method stub
+		
+		//Two manually defined sets using IntegerSet class for type
+		IntegerSet setA = new IntegerSet();
+		IntegerSet setB = new IntegerSet();
+		
+		//Add
+		System.out.println("Add");
+		System.out.println("Set A");
+	    setA.add(1);
+	    setA.add(2);
+	    setA.add(3);
+	    setA.add(56);	   	    
+	    System.out.println("\n");
+	    
+	    System.out.println("Add");
+	    System.out.println("Set B");
+	    setB.add(2);
+	    setB.add(3);
+	    setB.add(5);
+	    setB.add(17);
+	    System.out.println("\n");
+	    
+	    //Length
+//	    System.out.println("Length");
+//		System.out.println("Set A");
+//	    setA.length();
+//	    System.out.println("\n");
+//	    
+//	    System.out.println("Set B");
+//	    setB.length();
+//	    System.out.println("\n");
+	    
+	    
+	  //Equals (Change Values)
+//	    System.out.println("Equals");
+//		System.out.println("Set A");
+//	    setA.equals(setB);
+//	    System.out.println("\n");
+	    
+	    
+	    //Remove
+//	    System.out.println("Remove");
+//		System.out.println("Set A");
+//	    setA.remove(1);
+//	    System.out.println("\n");
+	    
+	    
+	    //Union
+//	    System.out.println("Union");
+//	    setA.union(setB);
+//	    System.out.println("\n");
+	    
+	    //Intersection
+//	    System.out.println("Intersection");
+//	    setA.intersect(setB);
+//	    System.out.println("\n");
+	  
+	    //Difference
+//	    System.out.println("Difference");
+//	    setA.diff(setB);
+//	    System.out.println("\n");
+	    
+	    //Clear
+//	    System.out.println("Clear");
+//	    System.out.println("Set A");
+//	    setA.clear();
+//	    System.out.println("\n");
+	    
+	    
+//	    System.out.println("Set B");
+//	    setB.clear();
+//	    System.out.println("\n");
+	    
+	    //isEmpty
+//	    System.out.println("isEmpty");
+//	    System.out.println("Set A");
+//	    setA.isEmpty();
+//	    System.out.println("\n");
+//	    
+//	    
+//	    System.out.println("isEmpty");
+//	    System.out.println("Set B");
+//	    setB.isEmpty();
+//	    System.out.println("\n");
+	    
+	    //Contains
+//	    System.out.println("Contains");
+//	    System.out.println("Set A");
+//	    setA.contains(2);
+//	    System.out.println("\n");
+	    
+	    //toString
+//	    System.out.println("Set A");
+//	    setA.toString();
+//	    System.out.println("\n");
+	    
+	    
+     	//Largest
+//	    System.out.println("Set A");
+//	    setA.largest();
+//	    System.out.println("\n");
+	    
+//	    System.out.println("Set B");
+//	    setB.largest();
+//	    System.out.println("\n");
+	    
+	    
+	    //Smallest
+//	    System.out.println("Set B");
+//	    setB.smallest();
+//	    System.out.println("\n");
+//	    
+//	    System.out.println("Set A");
+//	    setA.smallest();
+//	    System.out.println("\n");
+
+	}
+
+}
